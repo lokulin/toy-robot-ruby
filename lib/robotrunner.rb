4c3d9037ae9ca4cab9217f1dbf7faa29f08f7108
lokulin <lauchlin@lauchlin.com>
2015-04-22 22:18:21 +1000

    Make input validation a bit more robust

--- lib/robotrunner.rb
+++ lib/robotrunner.rb
@@ -5,7 +5,7 @@ class RobotRunner
   @@dirs = ["east","north","west","south"]
 
   def placeargs args
-    return args[0].to_i, args[1].to_i, @@dirs.index(args[2]).to_f*0.5, Table.new(0,0,4,4)
+    return args[0].to_i, args[1].to_i, @@dirs.index(args[2].downcase).to_f*0.5, Table.new(0,0,4,4)
   end
 
   def run file
@@ -13,10 +13,14 @@ class RobotRunner
     valid_cmds = Robot.public_instance_methods - Object.methods
 
     IO.foreach file do |line|
-      if /\A[A-Z]+( \d+,\d+,(NORTH|EAST|SOUTH|WEST)){0,1}\n\z/ === line
-        cmd, *args = line.downcase.split(/[\s,","]/)
-        *args = placeargs args if cmd == "place"
-        robot = robot.send cmd,*args if valid_cmds.index(cmd.to_sym)
+      match = %r"(?<=^)(MOVE$|LEFT$|RIGHT$|REPORT$|PLACE(?=\s?(\d+),(\d+),(NORTH|EAST|SOUTH|WEST)$))".match(line)
+      cmd, *args = match.captures unless match.nil?
+      case cmd
+      when "MOVE", "LEFT", "RIGHT", "REPORT"
+        robot = robot.send cmd.downcase
+      when "PLACE"
+        *args = placeargs args
+        robot = robot.send cmd.downcase,*args
       end
     end
   end
