03f37721e94640ae09f113e6e878915546b7b76b
lokulin <lauchlin@lauchlin.com>
2015-06-04 23:54:41 +1000

    Some basic smoke tests

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,117 +1,39 @@
-require_relative '../lib/robot'
-require_relative '../lib/table'
-require 'matrix'
+require_relative '../lib/robotrunner'
 
 describe Robot do
-  let(:robot) { Robot.new(Table.new(0,0,4,4)) }
+  let(:robotrunner) { RobotRunner.new }
+  before { ARGV.clear }
 
-  describe '#place' do
-    context 'on the table' do
-      it "is placed" do
-        expect(robot.place(0,0,'east')[0]).to eq(0)
-        expect(robot.place(0,0,'east')[1]).to eq(0)
-        expect(robot.place(0,0,'east')[2].first.name).to eq('east')
-      end
-    end
-    
-    context 'outside the table' do
-      it "is not placed" do
-        expect(robot.place(-1,0,'east')[0]).to eq(nil)
-        expect(robot.place(0,-1,'east')[0]).to eq(nil)
-        expect(robot.place(5,0,'east')[0]).to eq(nil)
-        expect(robot.place(0,5,'east')[0]).to eq(nil)
-      end
+  describe 'Run' do
+    it "Example 1 prints 0,1,NORTH" do
+      expect(STDOUT).to receive(:puts).with('0,1,NORTH')
+      ARGV[0] = "example/example1.txt"
+      robotrunner.run
     end
 
-    context 'using an invalid direction' do
-      it "is not placed" do
-        expect(robot.place(0,0,'up')[2]).to eq(nil)
-      end
-    end
-  end
-  
-  describe '#move' do
-    context 'a valid table location' do
-      before { robot.place(0,0,'north') }
-      it "moves" do 
-        expect(robot.move).to eq([0,1])
-      end
+    it "Example 2 prints 0,0,WEST" do
+      expect(STDOUT).to receive(:puts).with('0,0,WEST')
+      ARGV[0] = "example/example2.txt"
+      robotrunner.run
     end
 
-    context 'outside the table' do
-      before { robot.place(0,0,'south') }
-      it "doesn't move" do 
-        expect(robot.move).to eq([0,0])
-      end
-    end
-  end
-
-  describe '#left' do
-    context 'facing north' do
-      before { robot.place(0,0,'north') }
-      it "turns west" do
-        expect(robot.left.first.name).to eq('west')
-      end
+    it "Example 3 prints 3,3,NORTH" do
+      expect(STDOUT).to receive(:puts).with('3,3,NORTH')
+      ARGV[0] = "example/example3.txt"
+      robotrunner.run
     end
 
-    context 'facing west' do
-      before { robot.place(0,0,'west') }
-      it "turns south" do
-        expect(robot.left.first.name).to eq('south')
-      end
+    it "Example 4 prints nothing" do
+      expect(STDOUT).to_not receive(:puts)
+      ARGV[0] = "example/example4.txt"
+      robotrunner.run
     end
 
-    context 'facing south' do
-      before { robot.place(0,0,'south') }
-      it "turns east" do
-        expect(robot.left.first.name).to eq('east')
-      end
+    it "Example 5 prints 3,4,NORTH" do
+      expect(STDOUT).to receive(:puts).with('3,4,NORTH')
+      ARGV[0] = "example/example5.txt"
+      robotrunner.run
     end
-
-    context 'facing east' do
-      before { robot.place(0,0,'east') }
-      it "turns north" do
-        expect(robot.left.first.name).to eq('north')
-      end
-    end
-  end
-
-  describe '#right' do
-    context 'facing north' do
-      before { robot.place(0,0,'north') }
-      it "turns east" do
-        expect(robot.right.first.name).to eq('east')
-      end
-    end
-
-    context 'facing west' do
-      before { robot.place(0,0,'west') }
-      it "turns north" do
-        expect(robot.right.first.name).to eq('north')
-      end
-    end
-
-    context 'facing south' do
-      before { robot.place(0,0,'south') }
-      it "turns west" do
-        expect(robot.right.first.name).to eq('west')
-      end
-    end
-
-    context 'facing east' do
-      before { robot.place(0,0,'east') }
-      it "turns south" do
-        expect(robot.right.first.name).to eq('south')
-      end
-    end
-  end
-
-  describe '#report' do
-    before { robot.place(0,0,'north') }
-    it "prints the location and direction" do
-      expect(STDOUT).to receive(:puts).with('0,0,NORTH')
-      robot.report
-    end
-  end
+ end
 
 end
